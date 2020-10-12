267c1196cfa62509c578412cf77d1d4293d26d5b
Philip <philip_angelin@hotmail.com>
2020-10-12 18:42:17 +0200

    Fixed up 404 page

--- resources/js/components/NotFound.js
+++ resources/js/components/NotFound.js
@@ -1,9 +1,19 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
+import { createUseStyles } from 'react-jss';
 
 export default function NotFound() {
+    const styles = createUseStyles({
+        header: {
+            fontFamily: 'Barlow',
+            fontSize: '4rem',
+            margin: 'auto',
+        },
+    });
+    const classes = styles();
+
     return (
-        <div>
-            404
-        </div>
+        <h1 className={classes.header}>
+            404 Not Found
+        </h1>
     );
 }
